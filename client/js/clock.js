1e0dc4912c38258e92b65a0c1011fe9a246ee00d
Lladnaar <mark.randall@email.com>
2018-09-03 22:12:36 +0100

    Time settings prototype
    Created prototype time settings control.

--- client/js/clock.js
+++ client/js/clock.js
@@ -1,5 +1,51 @@
-window.onload = function () { init(); }
+import * as timer from './timer.js';
+
+var clock;
+
+window.addEventListener("load", function () { init(); } );
+
+function settings(event) {
+	var settings = document.getElementById("settings");
+	settings.style.visibility='visible';
+}
 
 function init() {
-	new Clock(document.getElementById('clock'))
+	clock = new timer.Clock(document.getElementById("clock"));
+	
+	window.addEventListener("keypress", settings);
+	
+	//************************
+	var now = new Date();
+	setClock(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes(), now.getSeconds());
+	document.getElementById("year").addEventListener("change", updateClock);
+	document.getElementById("month").addEventListener("change", updateClock);
+	document.getElementById("day").addEventListener("change", updateClock);
+	document.getElementById("hour").addEventListener("change", updateClock);
+	document.getElementById("minute").addEventListener("change", updateClock);
+	document.getElementById("second").addEventListener("change", updateClock);
+	//************************
 }
+
+//************************
+function setClock(y,m,d,h,n,s) {
+	var time = new Date(y,m,d,h,n,s);
+	document.getElementById("year").value = time.getFullYear();
+	document.getElementById("month").value = time.getMonth() + 1;
+	document.getElementById("day").value = time.getDate();
+	document.getElementById("hour").value = time.getHours();
+	document.getElementById("minute").value = time.getMinutes();
+	document.getElementById("second").value = time.getSeconds();
+}
+
+function updateClock() {
+	var time = new Date(
+		document.getElementById("year").value,
+		document.getElementById("month").value - 1,
+		document.getElementById("day").value,
+		document.getElementById("hour").value,
+		document.getElementById("minute").value,
+		document.getElementById("second").value
+	);
+	setClock(time.getFullYear(), time.getMonth(), time.getDate(), time.getHours(), time.getMinutes(), time.getSeconds());
+}
+//************************
--- client/js/timer.js
+++ client/js/timer.js
@@ -1,18 +1,64 @@
-class Clock {
+// format time for display
+function formatTimeFragment(f) {
+	if (isNaN(f)) {
+		return '\u2013\u2013';
+	}
+	else if (f < 10) {
+		return '0' + f;
+	}
+	else {
+		return f;
+	}
+}
+
+function formatTimeHMS(h, m, s) {
+	return formatTimeFragment(h) + ':' + formatTimeFragment(m) + ':' + formatTimeFragment(s);
+}
+
+function formatTimeHM(h, m, s) {
+	return formatTimeFragment(h) + ':' + formatTimeFragment(m);
+}
+
+function formatDurationHMS(h, m, s) {
+	return formatTimeFragment(h) + 'h' + formatTimeFragment(m) + 'm' + formatTimeFragment(s) + 's';
+}
+
+function formatDurationHM(h, m, s) {
+	return formatTimeFragment(h) + 'h' + formatTimeFragment(m) + 'm';
+}
+
+// timer class - Clock display
+
+export class Clock {
 	constructor(node) {
-		this.text = document.createTextNode('');
-		node.appendChild(this.text);
-		
+		// initialise time
+		this.offset = localStorage.getItem("TimeOffset") ? localStorage.getItem("TimeOffset") : 0;
+		this.formatTime = formatTimeHMS;
+
+		// create time text node
+		this.display = document.createTextNode('');
+		node.appendChild(this.display);
+
+		// initialise clock display and update
 		var clock = this;
+		this.tick();
 		this.timer = setInterval(function() { clock.tick(); }, 1000);
 	}
-	
-	format(h, m, s) {
-		return (h<10?'0':'') + h + ':' + (m<10?'0':'') + m + ':' + (s<10?'0':'') + s;
+
+	// calculate current time
+	getTime() {
+		return new Date(Date.now() + this.offset);
 	}
 
+	// set the timer
+	setTime(time) {
+		this.offset = time - Date.now()
+		localStorage.setItem("TimeOffset", this.offset);
+	}
+
+	// display current time
 	tick() {
-		var date = new Date();
-		this.text.textContent = this.format(date.getHours(), date.getMinutes(), date.getSeconds());
+		var now = this.getTime();
+		this.display.textContent = this.formatTime(now.getHours(), now.getMinutes(), now.getSeconds());
 	}
 }
